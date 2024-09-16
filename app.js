27baf471fd4ccfb661b07841c7d5862d6fcea966
huudungg <huudung2004kt@gmail.com>
2024-09-16 16:56:23 +0700

    add more route

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const { createServer } = require('node:http');
+const express = require('express')
+const app = express()
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.get('/', function (req, res) {
+    res.send('Hello World huu dung')
+})
+app.get("/test", function (req, res) {
+    res.send("this is a tesst")
+})
 
-const server = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World Huu Dung');
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(3000, () => {
+    console.log("server running on locallhost:3000")
+})
