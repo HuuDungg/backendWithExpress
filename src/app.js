3a5ed1b167b756f366a968316aa25a099dd00831
huudungg <huudung2004kt@gmail.com>
2024-09-16 17:23:35 +0700

    use env enviroment

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ const app = express();
 //config template engine
 app.set('views', path.join(__dirname, 'views/'));
 app.set('views engine', 'ejs');
-
+//env
+require('dotenv').config()
+port = process.env.PORT
+hostname = process.env.HOST_NAME
 
 //create route
 app.get('/', function (req, res) {
@@ -14,6 +17,6 @@ app.get("/test", function (req, res) {
     res.send("this is a tesst")
 })
 
-app.listen(8080, () => {
-    console.log("server running on locallhost:8080")
+app.listen(port, hostname, () => {
+    console.log(`server running on: ${hostname}:${port}`)
 })
