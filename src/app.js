d61d94ec6f305e6645e08a6f16f0fe257abbb630
huudungg <huudung2004kt@gmail.com>
2024-09-16 19:26:51 +0700

    load static file

--- src/app.js
+++ src/app.js
@@ -8,13 +8,16 @@ app.set('views engine', 'ejs');
 require('dotenv').config()
 port = process.env.PORT
 hostname = process.env.HOST_NAME
+//config static file
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 //create route
 app.get('/', function (req, res) {
     res.render("sample.ejs");
 })
 app.get("/test", function (req, res) {
-    res.send("this is a tesst")
+    res.send("this is a tesst hiih")
 })
 
 app.listen(port, hostname, () => {
