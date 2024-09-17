c86f4b5c531d92820811fb5276e0f3434ee2b75b
huudungg <huudung2004kt@gmail.com>
2024-09-17 17:23:21 +0700

    finlise before trans to nestjs

--- src/controller/customerController.js
+++ src/controller/customerController.js
@@ -1,23 +1,46 @@
 const Customer = require("../model/customer")
+const { upSingleFile } = require("../service/fileService")
 
 const getCustomer = (req, res) => {
-
+    res.status(200).json({
+        mss: 'success'
+    })
 }
 
 const getCustomerById = (req, res) => {
-
+    res.status(200).json({
+        mss: 'success'
+    })
 }
 
 const createCustomer = (req, res) => {
-
+    res.status(200).json({
+        mss: 'success'
+    })
 }
 
 const updateCustomer = (req, res) => {
-
+    res.status(200).json({
+        mss: 'success'
+    })
 }
 
 const delteteCustomer = (req, res) => {
+    res.status(200).json({
+        mss: 'success'
+    })
+}
+const uploadSingleFile = async (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    let result = await upSingleFile(req.files.image);
+    console.log(">>> check result: ", result)
+
+
+    return res.send("ok single")
 
 }
 
-module.exports = { getCustomer, getCustomerById, createCustomer, delteteCustomer, updateCustomer }
+module.exports = { getCustomer, getCustomerById, createCustomer, delteteCustomer, updateCustomer, uploadSingleFile }
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../model/user");
 const { CreateUser, getAllUser, updateUser, getUserById, deleteUserById } = require("../controller/apiController");
+const { getCustomer, getCustomerById, createCustomer, delteteCustomer, updateCustomer, uploadSingleFile } = require("../controller/customerController")
 const routerApi = express.Router();
 //create route api of user
 routerApi.get('/user', getAllUser);
@@ -10,10 +11,11 @@ routerApi.put('/user', updateUser);
 routerApi.delete('/user', deleteUserById);
 
 //create route api of customer
-routerApi.get("/customer");
-routerApi.get("/customer");
-routerApi.post("/customer");
-routerApi.put("/customer");
-routerApi.delete("/customer");
+routerApi.get("/customers", getCustomer);
+routerApi.get("/customer", getCustomerById);
+routerApi.post("/customer", createCustomer);
+routerApi.post("/file", uploadSingleFile);
+routerApi.put("/customer", updateCustomer);
+routerApi.delete("/customer", delteteCustomer);
 
 module.exports = routerApi;
